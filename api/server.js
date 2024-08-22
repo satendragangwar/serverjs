3df60ee665a5dfd17cedd5e8e81260c28761b178
satendra gangwar <101309019+satendragangwar@users.noreply.github.com>
2024-08-22 15:22:31 +0530

    Update server.js

--- api/server.js
+++ api/server.js
@@ -2,7 +2,12 @@
 const jsonServer = require('json-server')
 
 const server = jsonServer.create()
-
+server.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*'); // Allow any origin
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 // Uncomment to allow write operations
 // const fs = require('fs')
 // const path = require('path')
